6dd301aaa340f4b38414f038ed4b6474df81a21e
Sebastian Svensson <sebastian@rosmic.se>
2025-02-19 10:52:25 +0100

    MAUI profiles finish

--- ConcertBooking.MAUI/Profiles/CustomerProfile.cs
+++ ConcertBooking.MAUI/Profiles/CustomerProfile.cs
@@ -1,7 +1,7 @@
 ﻿using AutoMapper;
+using ConcertBooking.Data.DTO;
 using ConcertBooking.DTO;
 using ConcertBooking.MAUI.Models;
-using DMA_AU24_LAB2_Group4.Data.DTO;
 
 namespace ConcertBooking.MAUI.Profiles
 {
@@ -10,7 +10,7 @@ namespace ConcertBooking.MAUI.Profiles
         public CustomerProfile()
         {
             CreateMap<CustomerDTO, Customer>()
-            .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.CustomerID))
+            .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.CustomerId))
             .ForMember(dest => dest.FirstName, opt => opt.MapFrom(src => src.CustomerFirstName))
             .ForMember(dest => dest.LastName, opt => opt.MapFrom(src => src.CustomerLastName))
             .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email))
@@ -18,27 +18,26 @@ namespace ConcertBooking.MAUI.Profiles
             .ForMember(dest => dest.PasswordConfirmation, opt => opt.Ignore());
 
             CreateMap<Customer, CustomerDTO>()
-                .ForMember(dest => dest.CustomerID, opt => opt.MapFrom(src => src.Id))
+                .ForMember(dest => dest.CustomerId, opt => opt.MapFrom(src => src.Id))
                 .ForMember(dest => dest.CustomerFirstName, opt => opt.MapFrom(src => src.FirstName))
                 .ForMember(dest => dest.CustomerLastName, opt => opt.MapFrom(src => src.LastName))
                 .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email))
                 .ForMember(dest => dest.Password, opt => opt.MapFrom(src => src.Password));
 
-            CreateMap<Customer, UpdateCustomerDTO>()
+            CreateMap<Customer, CustomerRegisterDTO>()
+                .ForMember(dest => dest.FirstName, opt => opt.MapFrom(src => src.FirstName))
+                .ForMember(dest => dest.LastName, opt => opt.MapFrom(src => src.LastName))
+                .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email))
+                .ForMember(dest => dest.Password, opt => opt.MapFrom(src => src.Password));
+
+            CreateMap<Customer, CustomerUpdateDTO>()
                 .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
                 .ForMember(dest => dest.FirstName, opt => opt.MapFrom(src => src.FirstName))
                 .ForMember(dest => dest.LastName, opt => opt.MapFrom(src => src.LastName))
                 .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email))
                 .ForMember(dest => dest.Password, opt => opt.MapFrom(src => src.Password));
 
-            CreateMap<Customer, UpdateCustomerDTO>()
-                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
-                .ForMember(dest => dest.FirstName, opt => opt.MapFrom(src => src.FirstName))
-                .ForMember(dest => dest.LastName, opt => opt.MapFrom(src => src.LastName))
-                .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email))
-                .ForMember(dest => dest.Password, opt => opt.MapFrom(src => src.Password));
-
-            CreateMap<Customer, RegisterCustomerDTO>().ReverseMap();
+            CreateMap<Customer, CustomerRegisterDTO>().ReverseMap();
             CreateMap<Customer, LoginDTO>().ReverseMap();
         }
     }
--- ConcertBooking.MAUI/Profiles/PerformanceProfile.cs
+++ ConcertBooking.MAUI/Profiles/PerformanceProfile.cs
@@ -1,12 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AutoMapper;
+using ConcertBooking.DTO;
+using ConcertBooking.MAUI.Models;
 
 namespace ConcertBooking.MAUI.Profiles
 {
-    internal class PerformanceProfile
+    public class PerformanceProfile : Profile
     {
+        public PerformanceProfile()
+        {
+            CreateMap<Performance, PerformanceDTO>()
+                .ForMember(dest => dest.ID, opt => opt.MapFrom(src => src.Id))
+                .ForMember(dest => dest.PerformanceDate, opt => opt.MapFrom(src => src.PerformanceDateAndTime))
+                .ForMember(dest => dest.Venue, opt => opt.MapFrom(src => src.Venue))
+                .ForMember(dest => dest.City, opt => opt.MapFrom(src => src.City))
+                .ForMember(dest => dest.Country, opt => opt.MapFrom(src => src.Country))
+                .ForPath(dest => dest.ConcertTitle, opt => opt.MapFrom(src => src.Concert.Title));
+
+            CreateMap<PerformanceDTO, Performance>()
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.ID))
+                .ForMember(dest => dest.PerformanceDateAndTime, opt => opt.MapFrom(src => src.PerformanceDate))
+                .ForMember(dest => dest.Venue, opt => opt.MapFrom(src => src.Venue))
+                .ForMember(dest => dest.City, opt => opt.MapFrom(src => src.City))
+                .ForPath(dest => dest.Country, opt => opt.MapFrom(src => src.Country));
+        }
     }
 }
